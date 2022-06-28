4b7776f7ee561cd75f8325dd838aae3a8875dcec
Josh Edwards <jedwards404@gmail.com>
2022-06-28 14:12:19 -0400

    test

--- storybook-main/.storybook/preview.js
+++ storybook-main/.storybook/preview.js
@@ -1,16 +1,6 @@
 import '../styles/globals.css'
 import * as NextImage from 'next/image'
 
-/* import Image from 'next/image'
-
-Image.propTypes = {
-    unoptimized: null,
-}
-
-Image.defaultProps = {
-    unoptimized: true,
-} */
-
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
